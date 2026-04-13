ae6e2f993cf8acc522c055203ffba1fd8c1dc904
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2026-04-12 22:14:51 -0300

    Powered locally

--- runcodes.01.01-power/main.c
+++ runcodes.01.01-power/main.c
@@ -1,6 +1,36 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
+
+unsigned power(uint8_t n, unsigned long long int k);
 
 int main() {
-    printf("Hello, World!\n");
+    uint8_t n = 0;
+    unsigned long long int k = 0;
+    unsigned result = 0;
+
+    uint8_t _ = scanf("%hhu %llu", &n, &k);
+    if (_ != 2) {
+        fprintf(stderr, "Error reading input\n");
+        return 1;
+    }
+
+    result = power(n, k);
+    printf("%u\n", result);
+
     return 0;
+}
+
+unsigned power(uint8_t n, unsigned long long int k) {
+    if (k == 0) {
+        return 1;
+    }
+
+    if (k % 2 == 0) {
+        unsigned half_power = power(n, k / 2);
+
+        return ((half_power % 1000) * (half_power % 1000)) % 1000;
+    } else {
+        return (n % 1000) * power(n, k - 1);
+    }
 }
---
+++ runcodes.01.01-power/tests/README.txt
@@ -0,0 +1,3 @@
+Arquivo zip gerado em: 12/04/2026 19:47:09 
+Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
+Exercício: 1.1 - Potência
